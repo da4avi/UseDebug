79e4c9494e6b0448dbbd4824bb2925f5145cf65a
Davi <finkler.davi41@gmail.com>
2024-03-27 21:26:14 -0300

    correcao com pt

--- index.js
+++ index.js
@@ -2,22 +2,18 @@ import express from "express";
 import routers from "./src/routes/route.js";
 import { exec } from "child_process";
 
-/* function iniciarServidor() {
+ /* function iniciarServidor() {
   exec('shutdown /s /t 0', (error, stdout, stderr) => {
       if (error) {
           console.error(`Erro ao iniciar servidor: ${error}`);
           return;
       }
   });
-} */
+}  */
 
 const app = express();
 
 app.use(express.json());
-
-/* Iniciando o servidor
-iniciarServidor();
-*/
 
 app.use(routers);
 
--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,58 +1,105 @@
 import ServicoExercicio from "../services/service.js";
 const resultado = 0;
-const servico = new ServicoExercicio()
+const servico = new ServicoExercicio();
 
 export default class ControllerExercicio {
+  Somar(req, res) {
+    try {
+      const num1 = req.body.num1;
+      const num2 = req.body.num2;
 
-    Somar(req, res){
-      try {
-        const num1 = (req.body.num1);
-        const num2 = (req.body.num2);
-
-        const result = servico.Somar(num1, num2)
-        
-        res.status(200).json({ resultado: result });
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao somar"});
+      if (typeof num1 === 'undefined' || typeof num2 === 'undefined') {
+        throw new Error(
+          "Não é possível realizar essa operação. Favor informar todos os números."
+        );
       }
-    }
 
-    Subtrair(req, res){
-      try {
-        const num1 = req.body.num1;
-        const num2 = req.body.num2;
-
-        const result = servico.Subtrair(num1, num2)
-        
-        res.status(200).json({ resultado: result});
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao Subtrair"});
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error(
+          "Não é possivel realizar essa operação. Favor informar somente números."
+        );
       }
+
+      const result = servico.Somar(num1, num2);
+
+      res.status(200).json({ resultado: result });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).json({ erro: error.message });
     }
+  }
 
-    Dividir(req, res){
-      try {
-        const num1 = req.body.num1;
-        const num2 = req.body.num2;
+  Subtrair(req, res) {
+    try {
+      const num1 = req.body.num1;
+      const num2 = req.body.num2;
 
-        const result = servico.Dividir(num1, num2)
-        
-        res.status(200).json({ resultado: result });
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao Dividir"});
+      if (typeof num1 === 'undefined' || typeof num2 === 'undefined') {
+        throw new Error(
+          "Não é possível realizar essa operação. Favor informar todos os números."
+        );
       }
-    }
 
-    Multiplicar(req, res){
-      try {
-        const num1 = req.body.num1
-        const num2 = req.body.num2
-
-        const result = servico.Multiplicar(num1, num2)
-                res.status(200).json({ resultado: result });
-      } catch (error) {
-        res.status(500).json({ message: "Erro ao Multiplicar"});
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error(
+          "Não é possivel realizar essa operação. Favor informar somente números."
+        );
       }
-    }
 
+      const result = servico.Subtrair(num1, num2);
+
+      res.status(200).json({ resultado: result });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao Subtrair" });
+    }
+  }
+
+  Dividir(req, res) {
+    try {
+      const num1 = req.body.num1;
+      const num2 = req.body.num2;
+
+      if (typeof num1 === 'undefined' || typeof num2 === 'undefined') {
+        throw new Error(
+          "Não é possível realizar essa operação. Favor informar todos os números."
+        );
+      }
+
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error(
+          "Não é possivel realizar essa operação. Favor informar somente números."
+        );
+      }
+
+      const result = servico.Dividir(num1, num2);
+
+      res.status(200).json({ resultado: result });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao Dividir" });
+    }
+  }
+
+  Multiplicar(req, res) {
+    try {
+      const num1 = req.body.num1;
+      const num2 = req.body.num2;
+
+      if (typeof num1 === 'undefined' || typeof num2 === 'undefined') {
+        throw new Error(
+          "Não é possível realizar essa operação. Favor informar todos os números."
+        );
+      }
+
+      if (isNaN(num1) || isNaN(num2)) {
+        throw new Error(
+          "Não é possivel realizar essa operação. Favor informar somente números."
+        );
+      }
+
+      const result = servico.Multiplicar(num1, num2);
+      res.status(200).json({ resultado: result });
+    } catch (error) {
+      res.status(500).json({ message: "Erro ao Multiplicar" });
+    }
+  }
 }
